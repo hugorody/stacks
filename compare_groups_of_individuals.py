5894fef4dadc1360db70eb33f37c9834e74bc233
Hugo Rody <hugo.rody@unifesp.br>
2017-04-27 00:56:54 -0300

    Update compare_groups_of_individuals.py

--- compare_groups_of_individuals.py
+++ compare_groups_of_individuals.py
@@ -11,10 +11,15 @@ file2 = sys.argv[2] #batch_1.catalog.tags.tsv
 set_1 = list(line.strip() for line in open (file1, 'r'))
 set_2 = list(line.strip() for line in open (file2, 'r'))
 
-nameoutput = file1.replace(".txt", "")
+nameoutput = file1.replace(".lst", "")
+
+output0 = open(nameoutput+"_table.txt", "w")  #OUTPUT 0: TABLE
+output0.write("Desc Nshared_loci\n");
 
 #generate group catalog
-output1 = open(nameoutput+"_catalog.txt", "w")
+output1 = open(nameoutput+"_group.txt", "w")  #OUTPUT 1 GROUP CATALOG
+output1.write("CatalogID SampleID_LocusID\n");
+
 sizegroup = []
 for i in set_2:
 	if "#" not in i:
@@ -47,7 +52,7 @@ for i in set_2:
 			output1.write(catalogid+" "+",".join(["_".join([key, str(val)]) for key, val in newdict.items()])+"\n");
 
 
-print "The group shares",len(sizegroup),"loci"
+output0.write("group "+str(len(sizegroup))+"\n");
 
 #generate catalog for pair combinations among group members
 
@@ -59,7 +64,10 @@ for sampleid in pairs:
 	id1 = sampleid[0]
 	id2 = sampleid[1]
 	
-	output2 = open(nameoutput+"_"+id1+"_"+id2+".txt", "w")
+	sizepair = []
+	
+	output2 = open(nameoutput+"_"+id1+"_"+id2+".txt", "w")  #OUTPUT 2 INDIVIDUAL PAIR CATALOG
+	output2.write("CatalogID SampleID_LocusID\n");
 	
 	for i in set_2:
 		if "#" not in i:
@@ -83,3 +91,6 @@ for sampleid in pairs:
 				
 #				print catalogid,id1+"_"+dictsampleid[id1],id2+"_"+dictsampleid[id2]
 				output2.write(catalogid+" "+id1+"_"+dictsampleid[id1]+","+id2+"_"+dictsampleid[id2]+"\n");
+				sizepair.append(catalogid)
+	
+	output0.write(id1+","+id2+" "+str(len(sizepair))+"\n");
