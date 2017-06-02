6b75ecac1fc9b117ae946f140c7c61a48ab7f319
Hugo Rody <hugo.rody@unifesp.br>
2017-06-02 08:02:17 -0300

    Update pipeline_blastn_radtags_denovomap.sh

--- pipeline_blastn_radtags_denovomap.sh
+++ pipeline_blastn_radtags_denovomap.sh
@@ -11,14 +11,14 @@ echo "Run Stacks denovo_map?"; read rundenovomap
 
 #MASTER VARIABLES - DIRECTORIES AND FILES
 echo "ALL DIRECTORIES MUST BE SPECIFIED AS /path/to/dir/"
-echo "Enter the path for the directory to execute this pipeline:"; read outputsdir
-echo "Enter the path to the Individuals population file (format: individual <tab> int - Check Stacks manual for more information):" read individualpop
+echo "Enter the path for the outputs directory:"; read outputsdir
+echo "Enter the path to the Individuals population file (format: individual <tab> int - Check Stacks manual for more information):"; read individualpop
 
 #CONFIGURATION FOR BLASTN FILTER STEP
 if [ "$runblastn" = "1" ]; then
 echo "For the BLASTn Filter Step"
 echo "Enter directory for FASTq (*.fastq) raw reads files:"; read fqfilesdir
-echo "Reference FASTA file:" read reffasta
+echo "Reference FASTA file:"; read reffasta
 echo "Creating blastnfiles, fafiles and filterdfq directories in the output directory"
 mkdir "$outputsdir"fafiles    #FASTA FILES DIRECTORY
 mkdir "$outputsdir"filteredfq #FASTQ FILTERED FILES DIRECTORY
@@ -30,11 +30,12 @@ fi
 #CONFIGURATION FOR PROCESS_RADTAGS STEP
 if [ "$runprocessradtags" = "1" ]; then
 echo "For the PROCESS_RADTAGS  Step"
-echo "Final reads length for process_radtags (int):" read finalreadslength
-echo "Restriction enzyme used during GBS sequencing for process_radtags:" read restrictionenzyme
+echo "Final reads length for process_radtags (int):"; read finalreadslength
+echo "Restriction enzyme used during GBS sequencing for process_radtags:"; read restrictionenzyme
 echo "Path to the barcodes file (format: seq_barcode <tab> individual):"; read barcodesfile
+echo "Name for the output of Process_Radtags (processed individuals):"; read processed_individuals
 echo "Creating individuals and barcodes directories in the output directory"
-mkdir "$outputsdir"individuals   #Stacks INDIVIDUALS FILES DIRECTORY
+mkdir "$outputsdir""$processed_individuals"   #Stacks individuals FILES DIRECTORY
 mkdir "$outputsdir"barcodes
 else
 echo "PROCESS_RADTAGS STEP WONT BE EXECUTED"
@@ -43,17 +44,20 @@ fi
 #CONFIGURATION FOR DE NOVO MAP STEP
 if [ "$rundenovomap" = "1" ]; then
 echo "For the DE NOVO MAP  Step"
-echo "Enter a name (only the name) for the outputs from DE NOVO MAP step:" read denovooutput
+echo "Enter a name for this run to create a directory to write the outputs from DE NOVO MAP step:"; read denovooutput
 echo "Creating denovoouput and barcodes directories in the output directory"
 mkdir "$outputsdir""$denovooutput"   #DE NOVO map output result FILES DIRECTORY
 mkdir "$outputsdir"barcodes
-echo "Set the value (int) for the parameter -m:" mvalue
-echo "Set the value (int) for the parameter -M:" mmvalue
-echo "Set the value (int) for the parameter -n:" nvalue
-echo "Set the number (int) of threads to use -T:" nthreads
+echo "Set the value (int) for the parameter -m:"; read mvalue
+echo "Set the value (int) for the parameter -M:"; read mmvalue
+echo "Set the value (int) for the parameter -n:"; read nvalue
+echo "Set the number (int) of threads to use -T:"; read nthreads
 echo "This pipeline will not allow SNP calling from secondary reads!"
 if [ "$barcodesfile" = "" ]; then
 echo "Path to the barcodes file (format: seq_barcode <tab> individual):"; read barcodesfile
+fi
+if [ "$processed_individuals" = "" ]; then
+echo "Name for the output of Process_Radtags (processed individuals):"; read processed_individuals
 fi
 
 else
@@ -90,7 +94,9 @@ do
 done
 fi #end of BLASTn FILTER STEP
 
-#### PROCESS_RADTAGS step
+
+########################## PROCESS_RADTAGS step ################################
+
 if [ "$runprocessradtags" = "1" ]; then
 echo "Starting PROCESS_RADTAGS  Step"
 
@@ -105,13 +111,13 @@ done
 cat "$individualpop" | cut -f 1 | while read line
 do
   echo "executing process_radtags for $line"
-  process_radtags -t "$finalreadslength" -f "$outputsdir"filteredfq/"$line".fastq -i fastq -o "$outputsdir"individuals/ -b "$outputsdir"barcodes/"$line".txt -e "$restrictionenzyme" -r -q
-  mv "$outputsdir"individuals/process_radtags.log "$outputsdir"individuals/process_radtags_"$line".log  #rename log file for each fastq
+  process_radtags -t "$finalreadslength" -f "$outputsdir"filteredfq/"$line".fastq -i fastq -o "$outputsdir""$processed_individuals"/ -b "$outputsdir"barcodes/"$line".txt -e "$restrictionenzyme" -r -q
+  mv "$outputsdir""$processed_individuals"/process_radtags.log "$outputsdir""$processed_individuals"/process_radtags_"$line".log  #rename log file for each fastq
 done
 fi
 
-#### DE NOVO MAP step
+############################## DE NOVO MAP step #################################
 if [ "$rundenovomap" = "1" ]; then
 echo "Starting PROCESS_RADTAGS  Step"
-denovo_map.pl -t -T "$nthreads" -m "$mvalue" -M "$mmvalue" -n "$nvalue" -H -b 1 -S -O "$individualpop" -o "$outputsdir""$denovooutput"/ --samples "$outputsdir"individuals/
+denovo_map.pl -t -T "$nthreads" -m "$mvalue" -M "$mmvalue" -n "$nvalue" -H -b 1 -S -O "$individualpop" -o "$outputsdir""$denovooutput"/ --samples "$outputsdir""$processed_individuals"/
 fi
