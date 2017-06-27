dacb3789019763c221d1998edde2c40b917461e2
Hugo Rody <hugo.rody@unifesp.br>
2017-06-27 13:40:31 -0300

    Update get_fasta_shared_loci.py

--- get_fasta_shared_loci.py
+++ get_fasta_shared_loci.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
 
 import sys
-file1 = sys.argv[1] #ids list  <isolate ID><tab><stacks/sample ID>  note: isolate ID is normally the name of the fastq file
+file1 = sys.argv[1] #ids list  <file ID><tab><stacks/sample ID><tab><sample user code>  note: isolate ID is normally the name of the fastq file
 file2 = sys.argv[2] #loci list <catalog_shared_loci>
 file3 = sys.argv[3] #batch_1.catalog.tags.tsv
 
 #OPEN FILES TO CREATE DICTIONARIES
 with open(file1,'r') as set1:
     sampledict = dict()
+    samplevsisolateids = dict()
     for i in set1:
         i = i.split()
         isolate = i[0]
         idstack = int(i[1])
         sampledict[idstack] = isolate
+        samplevsisolateids[idstack] = i[2]
 set1.close()
 
 with open(file2,'r') as set2:
@@ -42,10 +44,9 @@ with open(file3,'r') as set3:
                     j = j.split("_")
                     sampleid = int(j[0])
                     locus_id = int(j[1])
-
-                    if sampleid not in idssamplelist:
-                        idssamplelist.append(sampleid)
-                        idslocilist.append(locus_id)
+                    #feed lists
+                    idssamplelist.append(sampleid)
+                    idslocilist.append(locus_id)
 
                 catalogids[locid] = idssamplelist
                 catalogloci[locid] = idslocilist
@@ -58,7 +59,7 @@ for i in locidict: #for each shared_loci in the input list
     output = open(str(i)+".fasta","w") #creates a fasta file
     ordersamples.append(i) #make a record of the order of loci being read
     for j in sampledict: #verifica quais samples possuem aquele loci consultando na lista de samples_IDs do catalogids
-        if j in catalogids[i]: #se a sample estiver no loci
+        if j in catalogids[i] and int(catalogids[i].count(j)) == 1: #se a sample estiver no loci and no more than one loci per the sample
             position = int(catalogids[i].index(j)) #captura a posicao da sample na lista dos catalogids
             samplelocus = catalogloci[i][position] #utiliza a posicao da sample para pegar sua respectiva locus id no dicionario catalogloci
 
@@ -68,8 +69,8 @@ for i in locidict: #for each shared_loci in the input list
                     line = line.split("\t")
                     if samplelocus == int(line[2]): #if sample locus id is found in line
                         sampleseq = line[9] #take the consensus sequence
-                        print ">"+str(j)+"_"+str(samplelocus)+" isolate "+str(sampledict[j])+" catalog loci "+str(i)+"\n"+sampleseq+"\n"
-                        output.write(">"+str(j)+"_"+str(samplelocus)+" isolate "+str(sampledict[j])+" catalog loci "+str(i)+"\n"+sampleseq+"\n"); #write in the shared loci fasta file, with sampleID_lociID as header
+                        print ">"+str(j)+"_"+str(samplelocus)+" isolate "+str(samplevsisolateids[j])+" catalog loci "+str(i)+"\n"+sampleseq+"\n"
+                        output.write(">"+str(j)+"_"+str(samplelocus)+" isolate "+str(samplevsisolateids[j])+" catalog loci "+str(i)+"\n"+sampleseq+"\n"); #write in the shared loci fasta file, with sampleID_lociID as header
 
                         if j in concater:  #feeds the concater dict with sampleID as keys and respective consensus sequences as values
                             seqseq = concater[j]+"MMM"+sampleseq
@@ -79,26 +80,24 @@ for i in locidict: #for each shared_loci in the input list
 
         else:
             sampleseq = "?" * 80
-            print ">"+str(j)+"_"+str(samplelocus)+" isolate "+str(sampledict[j])+" catalog loci "+str(i)+"\n"+sampleseq+"\n"
-            output.write(">"+str(j)+"_"+str(samplelocus)+" isolate "+str(sampledict[j])+" catalog loci "+str(i)+"\n"+sampleseq+"\n");
+            print ">"+str(j)+"_"+str(samplelocus)+" isolate "+str(samplevsisolateids[j])+" catalog loci "+str(i)+"\n"+sampleseq+"\n"
+            output.write(">"+str(j)+"_"+str(samplelocus)+" isolate "+str(samplevsisolateids[j])+" catalog loci "+str(i)+"\n"+sampleseq+"\n");
 
             if j in concater:
                 seqseq = concater[j]+"MMM"+sampleseq
                 concater[j] = seqseq
             else:
                 concater[j] = sampleseq
-
     output.close()
 
 #concatenated file
 output1 = open("concater.fasta","w")
 for i in concater.items():
-    output1.write(">"+str(i[0])+"\n"+i[1]+"\n");
+    output1.write(">"+samplevsisolateids[i[0]]+" Stacks_sample_ID "+str(i[0])+"\n"+i[1]+"\n");
 output1.close()
 
 #print order of concatenated locis
 output2 = open("concater_info.txt","w")
 for i in ordersamples:
-    i = i.rstrip()
     output2.write(str(i)+"\n");
 output2.close()
